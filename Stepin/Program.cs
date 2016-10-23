a54c3595ad7208c67b24349d3e5e7ef8fbca56da
IIITanbI <artyom.kuis@gmail.com>
2016-10-23 22:38:22 +0300

    edit

--- Stepin/Program.cs
+++ Stepin/Program.cs
@@ -1,18 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Stepin
 {
+    public static class Extentsion
+    {
+        public static IEnumerable<IEnumerable<T>> Permute<T>(this IEnumerable<T> sequence)
+        {
+            if (sequence == null)
+            {
+                yield break;
+            }
 
+            var list = sequence.ToList();
+
+            if (!list.Any())
+            {
+                yield return Enumerable.Empty<T>();
+            }
+            else
+            {
+                var startingElementIndex = 0;
+
+                foreach (var startingElement in list)
+                {
+                    var remainingItems = list.AllExcept(startingElementIndex);
+
+                    foreach (var permutationOfRemainder in remainingItems.Permute())
+                    {
+                        yield return startingElement.Concat(permutationOfRemainder);
+                    }
+
+                    startingElementIndex++;
+                }
+            }
+        }
+
+        private static IEnumerable<T> Concat<T>(this T firstElement, IEnumerable<T> secondSequence)
+        {
+            yield return firstElement;
+            if (secondSequence == null)
+            {
+                yield break;
+            }
+
+            foreach (var item in secondSequence)
+            {
+                yield return item;
+            }
+        }
+
+        private static IEnumerable<T> AllExcept<T>(this IEnumerable<T> sequence, int indexToSkip)
+        {
+            if (sequence == null)
+            {
+                yield break;
+            }
+
+            var index = 0;
+
+            foreach (var item in sequence.Where(item => index++ != indexToSkip))
+            {
+                yield return item;
+            }
+        }
+    }
     class AntProblem
     {
-        int max_iteration = 1000;
+        int max_iteration = 10000;
 
-        int n = 10;
-        int m = 10;
+        int n = 0;
+        int m = 0;
         List<int> bestWay = new List<int>();
         double bestLength = -1;
 
@@ -27,19 +90,41 @@ namespace Stepin
 
         Random random = new Random();
         List<Ant> ants = new List<Ant>();
+        double[][] nu = null;
+
+        public void Refresh()
+        {
+            int n = Distances.Length;
+            nu = new double[n][];
+            for (int i = 0; i < n; i++)
+                nu[i] = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    nu[i][j] = Math.Pow(trails[i][j], alpha) * Math.Pow(1.0 / Distances[i][j], beta);
+                }
+            }
+        }
         class Ant
         {
             AntProblem _parent = null;
-            public List<int> Way => _way.ToList();
+            public List<int> Way => _way;
 
-            public double Length { get; private set; } = -1;
+            public bool IsAlive { get; set; } = true;
+            public double Length { get; private set; } = 0;
             public int CurTown { get; private set; } = -1;
             public int StartTown { get; private set; } = -1;
 
             List<int> _way = new List<int>();
             bool[] _visited;
-            public Ant(AntProblem parent, int startTown = -1)
+
+
+
+            public Ant(AntProblem parent, int startTown)
             {
+
+
                 _parent = parent;
                 CurTown = startTown;
                 StartTown = startTown;
@@ -53,63 +138,59 @@ namespace Stepin
             {
                 if (town == -1 || _parent.Distances[CurTown][town] == -1)
                 {
-                    Length = -1;
+                    IsAlive = false;
                     return;
                 }
                 _visited[town] = true;
                 _way.Add(town);
                 Length += _parent.Distances[CurTown][town];
+                CurTown = town;
             }
             public void GoToNext()
             {
-                GoTo(NextTown);
+                GoTo(NextTown());
             }
             public void GoToStart()
             {
                 GoTo(StartTown);
             }
-            public int NextTown
+
+            public int NextTown()
             {
-                get
+                int n = _parent.Distances.Length;
+
+                int i = CurTown;
+                double summ = 0;
+
+                List<int> allowed = new List<int>();
+                for (int j = 0; j < n; j++)
+                    if (i != j && _parent.Distances[i][j] != -1 && !_visited[j])
+                        allowed.Add(j);
+
+                foreach (int j in allowed)
                 {
-                    int n = _parent.Distances.Length;
-
-                    int i = CurTown;
-                    double[] temp = new double[n];
-                    double summ = 0;
-
-                    List<int> allowed = new List<int>();
-                    for (int j = 0; j < n; j++)
-                        if (i != j && _parent.Distances[i][j] != -1 && !_visited[j])
-                            allowed.Add(j);
-
-                    foreach (int j in allowed)
-                    {
-                        temp[j] = Math.Pow(_parent.trails[i][j], _parent.alpha) * Math.Pow(1.0 / _parent.Distances[i][j], _parent.beta);
-                        summ += temp[j];
-                    }
-
-                    double[] prob = new double[n];
-                    foreach (int j in allowed)
-                        prob[j] = temp[j] / summ;
-
-                    double value = new Random().NextDouble();
-                    double curSumm = 0;
-                    foreach (int j in allowed)
-                    {
-                        curSumm += prob[j];
-                        if (curSumm >= value)
-                            return j;
-                    }
-
-                    return -1;
-                    //throw new Exception("No town to move next");
+                    summ += _parent.nu[i][j];
                 }
+
+                double[] prob = new double[n];
+                foreach (int j in allowed)
+                    prob[j] = _parent.nu[i][j] / summ;
+
+                double value = new Random().NextDouble();
+                double curSumm = 0;
+                foreach (int j in allowed)
+                {
+                    curSumm += prob[j];
+                    if (curSumm >= value)
+                        return j;
+                }
+
+                return -1;
             }
         }
 
 
-        void fillRandom()
+        void fillRandomDistances()
         {
             for (int i = 0; i < n; i++)
             {
@@ -133,13 +214,18 @@ namespace Stepin
                 }
             }
         }
-        public void solve()
+        public List<int> solve()
         {
-            Distances = CreateArray(n, n);
+            //Distances = CreateArray(n, n);
+            //fillRandomDistances();
+            n = Distances.Length;
+            m = n;
             trails = CreateArray(n, n, defaultTrail);
-            fillRandom();
+
+
             for (int t = 0; t < max_iteration; t++)
             {
+                Refresh();
                 ants.Clear();
                 for (int k = 0; k < m; k++)
                     ants.Add(new Ant(this, random.Next(0, n - 1)));
@@ -151,7 +237,9 @@ namespace Stepin
                     ants[k].GoToStart();
                 }
                 UpdateTrails();
+                UpdateBest();
             }
+            return bestWay;
         }
 
         void UpdateTrails()
@@ -173,7 +261,7 @@ namespace Stepin
         {
             foreach (Ant ant in ants)
             {
-                if ((bestLength == -1 && ant.Length != -1) || (bestLength != -1 && ant.Length < bestLength))
+                if (ant.IsAlive && (bestLength == -1 || ant.Length < bestLength))
                 {
                     bestLength = ant.Length;
                     bestWay = ant.Way;
@@ -194,13 +282,141 @@ namespace Stepin
             return array;
         }
     }
+
+
     class Program
     {
+        static void PrintMatrix(double[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    var value = Math.Round(matrix[i][j], 2);
+                    Console.Write(value.ToString().PadLeft(6));
+                }
+                Console.WriteLine();
+            }
+        }
+        static void PrintMatrixInFile(double[][] matrix, string file)
+        {
+            StringBuilder build = new StringBuilder();
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    var value = Math.Round(matrix[i][j], 2);
+                    build.Append(value.ToString().PadLeft(6));
+                }
+                build.AppendLine();
+            }
+            string str = build.ToString();
+            File.WriteAllText(file, str);
+        }
+        public static double[][] RandomDst(int n)
+        {
+            Random random = new Random();
+            double[][] distances = new double[n][];
+            for (int i = 0; i < n; i++)
+                distances[i] = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    var dst = random.Next(10, 20);
+                    distances[i][j] = dst;
+                    distances[j][i] = dst;
+                }
+            }
+            return distances;
+        }
         static void Main(string[] args)
         {
-            new AntProblem().solve();
+            string folder = @"C:\Users\1\Downloads\ALL_tsp";
+            string file = "a280";
+            //var distances = ParseDistances(Path.Combine(folder, file + ".tsp"));
+            var distances = RandomDst(5);
+            // var trueSolve = ParseSolve(Path.Combine(folder, file + ".opt.tour"));
+            var trueSolve = StupidSolve(distances);
+            PrintMatrix(distances);
+            PrintMatrixInFile(distances, "file.txt");
+            Stopwatch sw = Stopwatch.StartNew();
+            var antProblem = new AntProblem();
+            antProblem.Distances = distances;
+            var antSolve = antProblem.solve();
+            sw.Stop();
+            Console.WriteLine("Elapsed: "+ (double)sw.ElapsedMilliseconds/1000.0);
+
+            double res1 = 0;
+            for (int i = 0; i < antSolve.Count - 1; i++) res1 += distances[antSolve[i]][antSolve[i + 1]];
+
+            double res2 = 0;
+            for (int i = 0; i < trueSolve.Count - 1; i++) res2 += distances[trueSolve[i]][trueSolve[i + 1]];
+            Console.WriteLine(res1);
+            Console.WriteLine(res2);
             Console.WriteLine();
         }
 
+        static List<int> StupidSolve(double[][] distances)
+        {
+            int n = distances.Length;
+            List<int> way = new List<int>();
+            for (int i = 1; i < n; i++)
+                way.Add(i);
+
+            var permutes = way.Permute().Select(ie => ie.ToList()).ToList();
+
+            double bestLength = double.MaxValue;
+            List<int> bestWay = new List<int>();
+            foreach (var permute in permutes)
+            {
+                double temp = 0;
+                temp += distances[0][permute[0]];
+                for (int i = 0; i < permute.Count - 1; i++)
+                    temp += distances[permute[i]][permute[i + 1]];
+                temp += distances[permute.Last()][0];
+
+                if (temp < bestLength)
+                {
+                    bestLength = temp;
+                    bestWay = permute;
+                }
+            }
+            List<int> res = new List<int>();
+            res.Add(0);
+            res.AddRange(bestWay);
+            res.Add(0);
+            return res;
+        }
+        static double[][] ParseDistances(string file)
+        {
+            var allLines = File.ReadAllLines(file);
+            var lines = allLines.SkipWhile(s => s != "NODE_COORD_SECTION").Skip(1).TakeWhile(s => s != "EOF").ToList();
+
+            double[][] res = new double[lines.Count][];
+            List<Tuple<double, double>> points = new List<Tuple<double, double>>();
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var split = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                points.Add(new Tuple<double, double>(double.Parse(split[1]), double.Parse(split[2])));
+            }
+            for (int i = 0; i < points.Count; i++)
+            {
+                res[i] = new double[points.Count];
+                for (int j = 0; j < points.Count; j++)
+                {
+                    res[i][j] = Math.Sqrt(Math.Pow(points[i].Item1 - points[j].Item1, 2) + Math.Pow(points[i].Item2 - points[j].Item2, 2));
+                }
+            }
+            return res;
+        }
+        static List<int> ParseSolve(string file)
+        {
+            var allLines = File.ReadAllLines(file);
+            var lines = allLines.SkipWhile(s => s != "TOUR_SECTION").Skip(1).TakeWhile(s => s != "-1").ToList();
+
+            return lines.Select(l => int.Parse(l)).ToList();
+        }
     }
 }
---
+++ Tsehan/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsehan
+{
+    public static class Extentsion
+    {
+        public static IEnumerable<IEnumerable<T>> Permute<T>(this IEnumerable<T> sequence)
+        {
+            if (sequence == null)
+            {
+                yield break;
+            }
+
+            var list = sequence.ToList();
+
+            if (!list.Any())
+            {
+                yield return Enumerable.Empty<T>();
+            }
+            else
+            {
+                var startingElementIndex = 0;
+
+                foreach (var startingElement in list)
+                {
+                    var remainingItems = list.AllExcept(startingElementIndex);
+
+                    foreach (var permutationOfRemainder in remainingItems.Permute())
+                    {
+                        yield return startingElement.Concat(permutationOfRemainder);
+                    }
+
+                    startingElementIndex++;
+                }
+            }
+        }
+
+        private static IEnumerable<T> Concat<T>(this T firstElement, IEnumerable<T> secondSequence)
+        {
+            yield return firstElement;
+            if (secondSequence == null)
+            {
+                yield break;
+            }
+
+            foreach (var item in secondSequence)
+            {
+                yield return item;
+            }
+        }
+
+        private static IEnumerable<T> AllExcept<T>(this IEnumerable<T> sequence, int indexToSkip)
+        {
+            if (sequence == null)
+            {
+                yield break;
+            }
+
+            var index = 0;
+
+            foreach (var item in sequence.Where(item => index++ != indexToSkip))
+            {
+                yield return item;
+            }
+        }
+    }
+    class Machine
+    {
+        class Temp
+        {
+            public Task Task { get; set; } = null;
+            public int EndTime { get; set; } = -1;
+        }
+        Queue<Temp> q = new Queue<Temp>();
+
+        public bool IsEmpty => q.Count == 0;
+        public int EndTime => q.Count > 0 ? q.Peek().EndTime : -1;
+        public void AddTask(Task task, int time)
+        {
+            if (q.Count > 0)
+                q.Enqueue(new Temp() { Task = task });
+            else
+                q.Enqueue(new Temp() { Task = task, EndTime = time + task.Times[this] });
+        }
+
+        public Task RemoveTask()
+        {
+            Temp temp = q.Dequeue();
+            Task task = temp.Task;
+
+            if (q.Count > 0)
+            {
+                Temp _temp = q.Peek();
+                _temp.EndTime = temp.EndTime + _temp.Task.Times[this];
+            }
+            return task;
+        }
+
+        public void Reset()
+        {
+            q.Clear();
+        }
+    }
+    class Task
+    {
+        //machine - time
+        public Dictionary<Machine, int> Times = new Dictionary<Machine, int>();
+
+        public void SetTime(Machine machine, int time)
+        {
+            if (Times.ContainsKey(machine))
+                Times[machine] = time;
+            else Times.Add(machine, time);
+        }
+    }
+
+    class FlowTask
+    {
+        private static Random random = new Random(unchecked(Environment.TickCount * 31 + System.Threading.Thread.CurrentThread.ManagedThreadId));
+
+
+
+        void Init(int machineNumber, int taskNumber, List<Machine> machines, List<Task> tasks)
+        {
+            int m = machineNumber;
+            int n = taskNumber;
+
+            for (int i = 0; i < m; i++)
+            {
+                machines.Add(new Machine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                var task = new Task();
+                tasks.Add(task);
+                machines.ForEach(machine => task.SetTime(machine, random.Next(1, 10)));
+            }
+        }
+        void Init1(List<Machine> machines, List<Task> tasks)
+        {
+            int m = 2;
+    
+
+            for (int i = 0; i < m; i++)
+            {
+                machines.Add(new Machine());
+            }
+
+            Task task1 = new Task();
+            task1.SetTime(machines[0], 1);
+            task1.SetTime(machines[1], 10);
+
+            Task task2 = new Task();
+            task2.SetTime(machines[0], 3);
+            task2.SetTime(machines[1], 5);
+            tasks.Add(task2);
+            tasks.Add(task1);
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    var task = new Task();
+            //    tasks.Add(task);
+            //    machines.ForEach(machine => task.SetTime(machine, random.Next(10, 100)));
+            //}
+        }
+
+        public void Solve()
+        {
+            List<Machine> machines = new List<Machine>();
+            List<Task> tasks = new List<Task>();
+            Init(3, 9, machines, tasks);
+
+
+            List<int> results = new List<int>();
+            var permutates = tasks.Permute().Select(ie => ie.ToList()).ToList();
+            return;
+            foreach (var perm in permutates)
+            {
+                machines.ForEach(m => m.Reset());
+                var res = _solve(machines, perm);
+                //Console.WriteLine(res);
+                results.Add(res);
+            }
+            results.Sort();
+            results.ForEach(r => Console.WriteLine(r));
+        }
+        int _solve(List<Machine> machines, List<Task> tasks)
+        {
+            int curTime = 0;
+
+            int curTaskWaitStart = 0;
+            int LastTaskEnd = -1;
+
+            while (LastTaskEnd != tasks.Count - 1)
+            {
+                Machine machine = null;
+
+                if (machines.First().IsEmpty && curTaskWaitStart < tasks.Count)
+                {
+                    machine = machines.First();
+                    machine.AddTask(tasks[curTaskWaitStart], curTime);
+                    curTaskWaitStart++;
+                    continue;
+                }
+
+                machines.ForEach(m =>
+                {
+                    if (!m.IsEmpty && (machine == null || m.EndTime < machine.EndTime))
+                        machine = m;
+                });
+
+                if (machine != null)
+                {
+                    if (machine.EndTime == -1) throw new Exception("end time == -1");
+                    curTime = machine.EndTime;
+                    var task = machine.RemoveTask();
+
+                    var index = machines.IndexOf(machine);
+
+                    //end
+                    if (index == machines.Count - 1)
+                    {
+                        LastTaskEnd++;
+                    }
+                    else
+                    {
+                        Machine next = machines[index + 1];
+                        next.AddTask(task, curTime);
+                    }
+                }
+                else
+                {
+                    throw new Exception("machine is null");
+                }
+            }
+            return curTime;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 10; i++)
+                list.Add(i);
+
+            //var res = list.Permute().ToList();
+            //var res = list.Permute().Select(ie => ie.ToList()).ToList();
+            //Console.WriteLine(res.Count);
+            //return;
+            new FlowTask().Solve();
+        }
+    }
+}
